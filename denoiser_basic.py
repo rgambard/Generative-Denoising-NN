8b3b1d848fa697d79aef8217d6865224163876b4
Sara Meziane <smeziane-22@gpu2.enst.fr>
2024-12-17 21:09:39 +0100

    FID calculation added in the test function

--- denoiser_basic.py
+++ denoiser_basic.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from unet import UNet_Res, UNet, Denoiser, EnergyModel, ResidualUNet
 from utils import save_image, dotdict
-
+from metrics import preprocess_images , calculate_inception_score , InceptionV3 , calculate_inception_score_first_version , get_FID
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
@@ -75,17 +75,20 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 break
 
 nid= "vnoise"
-def test(model, device, test_loader):
+def test(model, device, test_loader, calculate_inception = False , Calculate_FID= False , N = 64):
     model.eval()
     test_loss = 0
     ctime = time.time()
+    j, k, h = test_loader.dataset[0][0].shape[-3:]  
+    collected_data = torch.empty((0, j, k, h), dtype=torch.float32, device=device)
     with torch.no_grad():
     #if True:
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             im = data
             loss, im_input, corr, pred_score= forward(data, model)
-
+            if collected_data.shape[0] < N:
+                collected_data = torch.cat((collected_data, im), dim=0)
             test_loss += loss.item()
 
     test_loss /= len(test_loader.dataset)
@@ -96,9 +99,17 @@ def test(model, device, test_loader):
     save_image(corr[:10],"im/corrected.jpg")
     save_image(orig[:10],"im/originals.jpg")
     gen_shape = list(im.shape)
-    gen_shape[0] = 64
+    gen_shape[0] = N
     gen_im = sampleLangevin(model, device, gen_shape)
     save_image(gen_im, "im/generated.jpg")
+    if calculate_inception :
+        normalized = gen_im.to(device)
+        mean,std = calculate_inception_score_first_version(normalized, batch_size=32, splits=10,dataset="MNIST")
+        print(f"Inception Score: {mean} ± {std}")
+    if Calculate_FID :
+        get_FID(collected_data,gen_im)
+
+    
 
 def sampleLangevin(model,device, im_shape, epsilon = 0.01, T=1, temp = 1.):
     with torch.no_grad():
@@ -122,7 +133,7 @@ TEST = True# set to true to load model from disk and only generate to test lange
 def main():
     global sigmas
     # Training settings
-    args_dict = {'batch_size' : 64, 'test_batch_size' :64, 'epochs' :200, 'lr' : 0.0002, 'gamma' : 0.98, 'no_cuda' :False, 'dry_run':False, 'seed': 1, 'log_interval' : 200, 'save_model' :True, 'only_test':False, 'model_path':"denoisermnist.pt", 'load_model_from_disk':False}
+    args_dict = {'batch_size' : 64, 'test_batch_size' :64, 'epochs' :200, 'lr' : 0.0002, 'gamma' : 0.98, 'no_cuda' :False, 'dry_run':False, 'seed': 1, 'log_interval' : 200, 'save_model' :True, 'only_test':True, 'model_path':"denoisermnist.pt", 'load_model_from_disk':False}
     if TEST:
         args_dict['load_model_from_disk'] = True
         args_dict['only_test'] = True
@@ -167,9 +178,9 @@ def main():
             transforms.Pad(2),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
-        dataset1 = datasets.MNIST('../data', train=True, download=True,
+        dataset1 = datasets.MNIST('data/', train=True, download=True,
         transform=transform)
-        dataset2 = datasets.MNIST('../data', train=False,
+        dataset2 = datasets.MNIST('data/', train=False,
         transform=transform)
         train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
         test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
